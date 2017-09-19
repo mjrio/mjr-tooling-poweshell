47511ce6ba5fbbf77166dfdd075e82f7d0a30aba
Thomas De Pauw <thdepauw@hotmail.com>
2017-09-19 12:10:00 +0200

    Update demo scripts

--- DemoScript.ps1
+++ DemoScript.ps1
@@ -2,18 +2,44 @@
 # Basic Commands
 # -----------------------------------
 
+#start powershell from explorer
+
 Get-help Get-Item
 Get-help Get-Item -ShowWindow
 
 Get-Command -Verb update
 
+Get-Item .\
+Get-ChildItem .\
+Get-ChildItem .\ -Recurse
+Get-Content .\demo-basic-commands\folder2\hello.txt 
+
+
 # -----------------------------------
 # Pipe lines Commands
 # -----------------------------------
 
-Get-ChildItem -Path C:\WINDOWS\System32 | Out-Host -Paging
-Get-ChildItem .\ -Recurse | Out-GridView -PassThru | foreach { Write-Host $_.Name -ForegroundColor Green }
+# Not implemented ?
+Get-ChildItem -Path C:\WINDOWS\System32 | Out-Host -Paging 
 
+# Show paths
+Get-ChildItem .\ -Recurse | Out-GridView
+
+# Print selected entries
+Get-ChildItem .\ -Recurse | Out-GridView -PassThru | foreach { Write-Host $_.Name -ForegroundColor Green } 
+
+# -----------------------------------
+# Aliases
+# -----------------------------------
+
+function Write-SayHello() {
+    Write-Host "Hello, how are you" -ForegroundColor Yellow
+}
+
+Set-Alias hello Write-SayHello
+
+Get-Alias
+Get-Alias he*
 
 # -----------------------------------
 # Variables
@@ -69,6 +95,7 @@ foreach ($item in Get-ChildItem .\ -Recurse)
 # -----------------------------------
 # Functions
 # -----------------------------------
+
 function HelloWorld() {
   Write-host "Hello world"
 }
@@ -127,10 +154,19 @@ function Calc-Something($a,$b) {
 # Basic module
 Import-Module -Name ".\BasicScriptModule.psm1" -Force
 
+# Verify loaded module
+Get-Module
+
 # Advanced module
-cd .\PSModules\PowerShell.Tools.SQL
+cd  C:\git\Tools.PowerShell\PSModules\PowerShell.Tools.SQL
 Import-Module .\PowerShell.Tools.SQL.psm1
 Get-Module
+
+# Get commands available in a module
+get-command -Module 'PowerShell.Tools.*'
+
+cd C:\git\cruise-basic-powershell
+
 
 # Module path
 $env:PSModulePath -split ';' #users path
@@ -149,6 +185,8 @@ Test-Path $profile
 
 $PROFILE | Format-List * -force #Show all paths to the profiles
 
+psedit $profile
+psedit $profile.CurrentUserAllHosts
 
 # -----------------------------------
 # $Powershell Core
@@ -166,3 +204,6 @@ dir
 
 # Invoke rest requests
 (Invoke-WebRequest -Uri 'http://jsonplaceholder.typicode.com/posts?userid=1' -Method Get).Content | ConvertFrom-Json
+
+# Multiple profiles are also supported
+$PROFILE | Format-List * -force #Show all paths to the profiles
